09f66d7269101bee2f660da1fb964aa0807aead6
Ben Follington <5009316+bfollington@users.noreply.github.com>
2025-07-15 10:57:03 +1000

    Add reflect.md and fix build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 mkdir -p out
-for file in bug-report.md feature.md progress.md resolve.md orient.md implement.md prototype.md code-review.md decompose.md next-up.md harden.md research.md debug.md spike.md; do
+for file in bug-report.md feature.md progress.md resolve.md orient.md implement.md prototype.md code-review.md decompose.md next-up.md harden.md reflect.md research.md debug.md; do
   deno run --allow-read --allow-write build.ts "$file" "out/$file"
 done
---
+++ out/reflect.md
@@ -0,0 +1,5 @@
+Let's reflect: $ARGUMENTS
+
+How did we get here? Did I write something, or did you read something that was confusing or misleading? Did you try to avoid embarassing me, or yourself along the way? There's no blame here, if you made a mistake it's because the instructions were poor.
+
+How would you, with hindsight, alter the instructions or approach we took here in the future? What steps can we take immediately?
